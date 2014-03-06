04bb823b595bf8977c38196c19ba0c8dd65103fa
adambarquier <alu53729143q@ieselcaminas.org>
2014-03-06 19:51:07 +0100

    Primera subida examen GitHub

---
+++ Ejercicio7GitHub/src/camion/Caja.java
@@ -0,0 +1,28 @@
+package camion;
+
+public class Caja {
+	
+	public String idCaja;		// id (codigo de barras) qupackage camion;e identifica a la caja	
+	public String contenido;	// descripcion del contenido de la caja
+	public double peso;			// peso de la caja y su contenido
+	
+	
+	
+	
+	public Caja(String idCaja, String contenido, double peso) {
+		super();
+		this.idCaja = idCaja;
+		this.peso = peso;
+		this.contenido = contenido;
+	}
+
+
+	@Override
+	public String toString() {
+		return "\n\tCaja [" + "idCaja=" + idCaja + " ,contenido=" + contenido + ", peso=" + peso + "]";
+	}
+	
+	
+	
+
+}
